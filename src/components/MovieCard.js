30e63cc37400336749e784ffa5be85c1cf74d86c
kadircanincekalan <kadircanincekalan@gmail.com>
2022-01-14 23:54:40 +0300

    v2

---
+++ src/components/MovieCard.js
@@ -0,0 +1,15 @@
+const MovieCard = () => {
+
+    return(
+        <div className="movie">
+            <div>
+                <h3>Title</h3>
+            </div>
+            <div className="movie-over">
+                <h2>Overview</h2>
+            </div>
+        </div>
+    )
+}
+
+export default MovieCard;
